0df0f1d20ec5027383685a576dc7c9e88a92448d
TINOTENDA099 <tinomutonhori447@gmail.com>
2025-05-25 10:42:50 +0200

    Add files via upload

---
+++ Module_04TINMUT25149_PTO2503_GROUP-B_Tinotenda_Mutonhori_SDF04.h
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Footer</title>
+</head>
+<header>
+    <h1>Subscribe to our news letter for updates</h1>
+   <h2>Quick link</h2>
+    <a href="">Home</a>
+    <a href="">Who we are</a>
+    <a href="">Our Philosophy</a>
+    <H3>Technologies</H3>
+    <a href="">HTML</a>
+    <a href="">Css</a>
+    <a href="">JavaScript</a>
+    <hr>
+</header>
+ 
+<body>
+    <footer class="bg-[#659f8f] dark:bg-[#313131]">
+        <div class="container px-6 py-12 mx-auto">
+            <div class="flex">
+                <a href="#">
+                    <img class="w-auto h-7" src="https://codespace-assets.global.ssl.fastly.net/wp/assets/website/codespace-primary-logo-light.svg" alt="CodeSpace Logo">
+                </a>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
